a0e9013b21ab626268e9e750f5947782d8625e47
Jeremy Allison <jallison@ciq.com>
2023-07-06 10:53:32 -0700

    Use fail_drbg() probe in crypto_rng_generate() and crypto_rng_reset().
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- crypto/rng.c
+++ crypto/rng.c
@@ -52,6 +52,11 @@ int crypto_rng_reset(struct crypto_rng *tfm, const u8 *seed, unsigned int slen)
 		seed = buf;
 	}
 
+	if (fail_drbg(tfm->base.__crt_alg, "reset")) {
+		err = -EINVAL;
+		goto out;
+	}
+
 	err = crypto_rng_alg(tfm)->seed(tfm, seed, slen);
 out:
 	kfree_sensitive(buf);
--- include/crypto/rng.h
+++ include/crypto/rng.h
@@ -140,6 +140,9 @@ static inline int crypto_rng_generate(struct crypto_rng *tfm,
 				      const u8 *src, unsigned int slen,
 				      u8 *dst, unsigned int dlen)
 {
+	if (fail_drbg(tfm->base.__crt_alg, "generate")) {
+		return -EINVAL;
+	}
 	return crypto_rng_alg(tfm)->generate(tfm, src, slen, dst, dlen);
 }
 
